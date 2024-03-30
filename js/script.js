a4419f91f22da7b07e6b21057f7691042126b769
kedoshim <abraaodpaula@gmail.com>
2024-03-29 22:06:11 -0300

    Remove multiple rounds logic

--- js/script.js
+++ js/script.js
@@ -7,103 +7,87 @@ const P2_VICTORY = -1;
 const DRAW = 0;
 
 function main() {
-  console.log("ROCK-PAPER-SCISSORS 5 ROUND DEATHMATCH - START!");
+    console.log("ROCK-PAPER-SCISSORS 5 ROUND DEATHMATCH - START!");
 
-  let score = 0;
+    let score = 0;
 
-  for (let i = 0; i < 5; i++) {
-    let player_choice = prompt(`What is your ${i + 1}° choice?`).toLowerCase();
+    let player_choice = prompt(`What is your choice?`).toLowerCase();
 
     if (![ROCK, PAPER, SCISSORS].includes(player_choice)) {
-      console.log(
-        `invalid choice! You chose ${player_choice}\nWrite 'rock','paper' or 'scissors'`
-      );
-      i -= 1;
-      continue;
+        console.log(
+            `invalid choice! You chose ${player_choice}\nWrite 'rock','paper' or 'scissors'`
+        );
+        main();
+        return;
     }
 
     score += Round(player_choice);
 
-    console.log(`Score: ${score}`);
-  }
-  if (score == 5 || score == -5) {
-    console.log("PERFECT");
-  }
-  if (score > 0) {
-    console.log("YOU WON THE DEATHMATCH!");
-  }
-  if (score < 0) {
-    console.log("YOU LOSE!");
-  } else {
-    console.log("YOU BOOOORING");
-  }
+    if (score == P1_VICTORY) {
+        console.log("YOU WON THE DEATHMATCH!");
+    }
+    if (score == P2_VICTORY) {
+        console.log("YOU LOSE!");
+    } else {
+        console.log("YOU DRAW!");
+    }
 }
 
 function Round(player_choice) {
-  let computer_choice = getComputerChoice();
-  console.log(`Computer played ${computer_choice}`);
+    let computer_choice = getComputerChoice();
+    console.log(`Computer played ${computer_choice}`);
 
-  let result = getWinner(player_choice, computer_choice);
+    let result = getWinner(player_choice, computer_choice);
 
-  if (result == P1_VICTORY)
-    console.log(
-      `You won the round! ${player_choice} wins over ${computer_choice}\n+1 point!`
-    );
-  if (result == P2_VICTORY)
-    console.log(
-      `You lost the round! ${player_choice} losses to ${computer_choice}\n-1 point!`
-    );
-  if (result == DRAW) console.log("Its a Draw!");
-
-  return result;
+    return result;
 }
 
 function getWinner(choice1, choice2) {
-  switch (choice1) {
-    case ROCK:
-      switch (choice2) {
+    switch (choice1) {
         case ROCK:
-          return DRAW;
+            switch (choice2) {
+                case ROCK:
+                    return DRAW;
+                case PAPER:
+                    return P2_VICTORY;
+                case SCISSORS:
+                    return P1_VICTORY;
+                default:
+                    return ERROR;
+            }
         case PAPER:
-          return P2_VICTORY;
+            switch (choice2) {
+                case ROCK:
+                    return P1_VICTORY;
+                case PAPER:
+                    return DRAW;
+                case SCISSORS:
+                    return P2_VICTORY;
+                default:
+                    return ERROR;
+            }
         case SCISSORS:
-          return P1_VICTORY;
-        default:
-          return ERROR;
-      }
-    case PAPER:
-      switch (choice2) {
-        case ROCK:
-          return P1_VICTORY;
-        case PAPER:
-          return DRAW;
-        case SCISSORS:
-          return P2_VICTORY;
-        default:
-          return ERROR;
-      }
-    case SCISSORS:
-      switch (choice2) {
-        case ROCK:
-          return P2_VICTORY;
-        case PAPER:
-          return P1_VICTORY;
-        case SCISSORS:
-          return DRAW;
-        default:
-          return ERROR;
-      }
-  }
+            switch (choice2) {
+                case ROCK:
+                    return P2_VICTORY;
+                case PAPER:
+                    return P1_VICTORY;
+                case SCISSORS:
+                    return DRAW;
+                default:
+                    return ERROR;
+            }
+    }
 }
 
 function getComputerChoice() {
-  if (Math.random() < 0.3) {
-    return ROCK;
-  } else if (Math.random() < 0.6) {
-    return PAPER;
-  } else {
-    return SCISSORS;
-  }
+    if (Math.random() < 0.3) {
+        return ROCK;
+    } else if (Math.random() < 0.6) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
 }
 
 main();
